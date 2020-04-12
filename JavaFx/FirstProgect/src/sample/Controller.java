7f5105eaf98c003494b0b50c05986aa9b3076666
RushianHaker <Maxim Ivanov>
2020-04-12 20:06:12 +0300

    написал калькулятор,пока что без ограничения по вводу чисел в текстовое поле.

--- JavaFx/FirstProgect/src/sample/Controller.java
+++ JavaFx/FirstProgect/src/sample/Controller.java
@@ -1,19 +1,30 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 
 public class Controller {
+
+
+    int number = 0;
+
+    @FXML
+    private Label NumbersLabel;
 
     @FXML
     private Button numberButton_7;
 
     @FXML
-    private Button numberButton_6;
+    private Button numberButton_4;
 
     @FXML
     private Button numberButton_1;
+
+    @FXML
+    private Button numberButton_0;
 
     @FXML
     private Button numberButton_8;
@@ -28,23 +39,71 @@ public class Controller {
     private Button numberButton_9;
 
     @FXML
-    private Button numberButton_4;
+    private Button numberButton_6;
 
     @FXML
     private Button numberButton_3;
 
     @FXML
-    private Button ranvier_element;
+    private Button btnDot;
 
     @FXML
-    private Button X_element;
+    private Button check;
 
     @FXML
-    private Button minys_element;
+    private Button element_X;
 
     @FXML
-    private Button plus_element;
+    private Button element_minys;
 
-    public void onClickMethod(javafx.event.ActionEvent actionEvent) {
+    @FXML
+    private Button element_plus;
+
+
+    public void none(){
+        for (int i = 0; i < 7; i++) {
+            //onClickMethod();
+        }
+    }
+
+    public void onClickMethod(ActionEvent actionEvent) {
+
+        NumbersLabel.setText("0");
+
+        if (numberButton_1.getId().equals("numberButton_1")) {
+            NumbersLabel.setText("1");
+        } else if (numberButton_2.getId().equals("numberButton_2")) {
+            NumbersLabel.setText("2");
+        } else if (numberButton_3.getId().equals("numberButton_3")) {
+            NumbersLabel.setText("3");
+        } else if (numberButton_4.getId().equals("numberButton_4")) {
+            NumbersLabel.setText("4");
+        } else if (numberButton_5.getId().equals("numberButton_5")) {
+            NumbersLabel.setText("5");
+        } else if (numberButton_6.getId().equals("numberButton_6")) {
+            NumbersLabel.setText("6");
+        } else if (numberButton_7.getId().equals("numberButton_7")) {
+            NumbersLabel.setText("7");
+        } else if (numberButton_8.getId().equals("numberButton_8")) {
+            NumbersLabel.setText("8");
+        } else if (numberButton_9.getId().equals("numberButton_9")) {
+            NumbersLabel.setText("9");
+        }
+    }
+
+    public void deem(int num, int num2) {
+
+        char x = '/';
+        int answer = 0;
+
+        if (x == '+') {
+            answer = num + num2;
+        } else if (x == '-') {
+            answer = num - num2;
+        } else if (x == '*') {
+            answer = num * num2;
+        } else {
+            answer = num / num2;
+        }
     }
 }
--- JavaFx/FirstProgect/src/sample/Main.java
+++ JavaFx/FirstProgect/src/sample/Main.java
@@ -13,8 +13,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("HelloButton.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Calculator");
         try {
             primaryStage.setScene(new Scene(root, 340, 600));
             primaryStage.show();
