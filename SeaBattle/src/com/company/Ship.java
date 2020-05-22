11798d092d3081d5255901b5ecd2bf587d3b6d70
RushianHaker <Maxim Ivanov>
2020-05-22 22:29:40 +0300

    Переделал условие проверки выйграша в задаче - SeaBatlle

--- SeaBattle/src/com/company/Ship.java
+++ SeaBattle/src/com/company/Ship.java
@@ -1,34 +1,21 @@
 package com.company;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Ship{
+public class Ship {
 
     char[][] map = new char[4][4];
+    char[][] exampleList = {{'0', '0', '0', '0'}, {'0', '0', '0', '0'}, {'0', '0', '0', '0'}, {'0', '0', '0', '0'}};
+
     String a = "0";
     String b = "*";
     String sum = a + b;
     int win = 0;
+
     Random rnd = new Random();
     Scanner scanner = new Scanner(System.in);
-
-    public int checkWin() {
-        if (map[0][0] == '0' && map[0][1] == '0' && map[0][2] == '0' &&
-                map[1][0] == '0' && map[1][1] == '0' && map[1][2] == '0' &&
-                map[2][0] == '0' && map[2][1] == '0' && map[2][2] == '0' &&
-                map[0][0] == '0' && map[1][0] == '0' && map[2][0] == '0' &&
-                map[0][1] == '0' && map[1][1] == '0' && map[2][1] == '0' &&
-                map[0][2] == '0' && map[1][2] == '0' && map[2][2] == '0' &&
-                map[0][0] == '0' && map[1][1] == '0' && map[2][2] == '0' &&
-                map[0][2] == '0' && map[1][1] == '0' && map[2][0] == '0') {
-            win = 1;
-            System.out.println("YOU WIN");
-            System.exit(0);
-        }
-        return win;
-    }
-
 
     public void generateOnMap() {
         System.out.println("///////////////////");
@@ -51,6 +38,7 @@ public class Ship{
         do {
             if (map[x][y] == '*') {
                 map[x][y] = '0';
+                System.out.println("Корабль потоплен!");
                 updateMap();
                 playerShout();
             } else {
@@ -62,8 +50,9 @@ public class Ship{
     }
 
     public void updateMap() {
+        System.out.println(" ");
         System.out.println("///////////////////");
-        System.out.println("Это игровое поле...");
+        System.out.println("Поле битвы...");
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 System.out.print(map[i][j] + "\t");
@@ -72,4 +61,32 @@ public class Ship{
         }
         checkWin();
     }
+
+
+    public int checkWin() {
+        if (Arrays.deepEquals(map, exampleList)) {
+            win = 1;
+            System.out.println("Поздравляем, ты потапил все коробли");
+            System.out.println("YOU WIN");
+            System.exit(0);
+        }
+        return win;
+    }
 }
+
+/*
+if (map[0][0] == '0' && map[0][1] == '0' && map[0][2] == '0' &&
+                map[1][0] == '0' && map[1][1] == '0' && map[1][2] == '0' &&
+                map[2][0] == '0' && map[2][1] == '0' && map[2][2] == '0' &&
+                map[0][0] == '0' && map[1][0] == '0' && map[2][0] == '0' &&
+                map[0][1] == '0' && map[1][1] == '0' && map[2][1] == '0' &&
+                map[0][2] == '0' && map[1][2] == '0' && map[2][2] == '0' &&
+                map[0][0] == '0' && map[1][1] == '0' && map[2][2] == '0' &&
+                map[0][2] == '0' && map[1][1] == '0' && map[2][0] == '0'){
+
+                win = 1;
+                System.out.println("YOU WIN");
+                System.exit(0);
+
+                }
+ */
