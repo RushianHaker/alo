ea4badeeed5347f257c29f47409bea7fc3c8e22c
Ivanov Maxim <ivanov2001m352g@mail.ru>
2020-09-28 21:48:08 +0300

    Исправил архитектуру, добавил последний структурный паттерн и  новый пакет с поведенческим патерном...

---
+++ Adapter/src/structual/flyweight/Main.java
@@ -0,0 +1,32 @@
+package flyweight;
+
+
+
+/*
+        Паттерн - описывает правильное применение техники создания разделяемых объектов, для получения
+        возмтжности эффективного использования большого числа обектов
+
+        Огранизует разделения для эффективной поддержки большого числа объектов, на разделяемые и не разделяемые.
+
+        -> Организует работу с разделяемыми объектами
+*/
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        int extrinsicState = 0;
+
+        Flyweigth flyweigth = null;
+        FlyweigthFactory flyweigthFactory = new FlyweigthFactory();
+
+        flyweigth = flyweigthFactory.getFlyweigth("1");
+        flyweigth.operation(extrinsicState);
+
+        flyweigth = flyweigthFactory.getFlyweigth("15");
+        flyweigth.operation(extrinsicState);
+
+        flyweigth = new UnsharedConcreateFlyweight();
+        flyweigth.operation(extrinsicState);
+    }
+}
